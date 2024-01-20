5ae620dcb4bcab264ba622a1a7759da21930fa1e
Sithum Suraweera <130219154+suraweera-AGSS@users.noreply.github.com>
2024-01-20 20:10:33 +0530

    Add files via upload

---
+++ apiPractice.js
@@ -0,0 +1,14 @@
+function getJoke(){
+    const apiUrl="https://api.chucknorris.io/jokes/random";
+
+    const jokeElement = document.getElementById("jokeContainer");
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            jokeElement.innerHTML= data.value;
+        })
+        .catch(error => {
+            console.log("Error Found!")
+        });
+}
